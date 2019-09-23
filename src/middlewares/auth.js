d5614aa53b246c60bb1834cf4037aeaab4df0483
Edgar Lima <edgar.lima@MacBook-Pro-de-Edgar.local>
2019-09-22 22:43:27 -0300

    Commit inicial

---
+++ src/middlewares/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader){
+        res.status(401).send( { error:  'No token provider'});
+    };
+
+    const parts = authHeader.split(' ');
+
+    if (!parts.lenght === 2){
+        res.status(401).send ( { error: 'Token error'});
+    };
+
+     const [ scheme, token ] = parts;
+
+     if (!/^Bearer$/i.test(scheme)){
+        res.status(401).send( { error: 'Token malformatted'});
+     };
+
+     jwt.verify(token, authConfig.secret, (err, decoded) => {
+          if (err){
+              res.status(401).send( { error: 'Token invalid'});
+          };
+
+          req.userId = decoded.params.id;
+          
+          console.log('logado', decoded);
+
+          return next();
+     });
+}; 
